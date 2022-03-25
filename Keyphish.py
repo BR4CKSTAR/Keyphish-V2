cb4dfffa99aa3234d8172c8c47811c7aa70410a9
Mr-Star21 <101686388+Mr-Star21@users.noreply.github.com>
2022-03-24 22:53:01 -0500

    Update Keyphish.py

--- Keyphish.py
+++ Keyphish.py
@@ -74,7 +74,7 @@ def ngrok():
 
 os.system("sudo apt install gnome-terminal php python3 python3-pip")
 clearConsole()
-os.system("pip install pyngrok")
+os.system("curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok   ")
 clearConsole()
 os.system("sudo pyngrok")
 clearConsole()
